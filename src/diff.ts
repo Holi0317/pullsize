9279b5c5843e4e6b906e66de2861b9e090a4bef8
Hollis Wu <whyhollis@gmail.com>
2024-02-17 19:49:56 -0500

    Fix NaN in comment

--- src/diff.ts
+++ src/diff.ts
@@ -4,5 +4,5 @@ export function genDiffSummary(diffFile: string) {
   const diff = parseGitDiff(diffFile);
 
   // TODO: Implement diff summary and return label as well
-  return "```json\n" + +JSON.stringify(diff) + "\n```";
+  return "```json\n" + JSON.stringify(diff) + "\n```";
 }
